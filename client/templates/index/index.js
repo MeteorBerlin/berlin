03c26058ce2f16a77927af86b9b402fa9c1a1b5d
Finn Frotscher <finnfrotscher@Finns-MacBook-Pro.local>
2015-12-17 14:53:08 +0100

    Jobs -Homepage and Contact

--- client/templates/index/index.js
+++ client/templates/index/index.js
@@ -3,17 +3,23 @@ Template.index.helpers({
     var events = Template.instance().events.get()
     return Template.instance().events.get()
   },
+  jobs: function () {
+    console.log("found jobs:   " +Jobs.find({}, {sort:{createdAt:-1}}).desc)
+    return Jobs.find({}, {sort:{createdAt:-1}}).fetch()
+  },
   //convert event time to date object since moFormat doesn't accept millisecond timestamps
   date: function (time) {
-    console.log(time)
+    //console.log(time)
     return moment(time).format('LLLL')
   }
 });
 
 Template.index.onCreated(function () {
+  this.subscribe('jobs')
   var instance = this
   //reactive var to fetch events from meetup.com
   instance.events = new ReactiveVar()
+
   //get meetup events from server method
   Meteor.call('getMeetupEvents', function (error, result) {
     if (error)
